1d9c03b7fb36428f73ec3cd0fe21fd4d72d3325b
nicholas-p1 <nicholas.pfaff19@ic.ac.uk>
2021-02-07 17:35:52 +0000

    Add minimal tcp client

---
+++ tcp_minimal/tcp_client_minimal.cpp
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    sockaddr_in saddr = {AF_INET, htons(4000), 0x0100007f};
+
+    while(true) {
+        int s = socket(AF_INET, SOCK_STREAM, 0);
+
+        connect(s, (sockaddr*)&saddr, sizeof(saddr));
+
+        uint32_t value = 1;
+        send(s, &value, 4, 0);
+
+        uint32_t payload;
+        recv(s, &payload, 4, 0);
+
+        cout << "State=" << payload <<endl;
+
+        close(s);
+    }
+}
