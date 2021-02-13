11850460e1f3f13e41ff66166cc9c8be8ddd8f43
nicholas-p1 <nicholas.pfaff19@ic.ac.uk>
2021-02-13 22:36:23 +0000

    Add python3.6 installation to machine-setup.sh

--- machine-setup.sh
+++ machine-setup.sh
@@ -8,7 +8,12 @@ g++ \
 git \
 make \
 python3 \
-curl
+curl \
+
+# Making sure python3.6 is installed
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6
 
 # Changing default editor to vim
 sudo update-alternatives --set editor $(which vim.basic)
